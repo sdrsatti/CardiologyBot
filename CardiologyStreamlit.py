575ba95550abf275744f3202c117a0853ebcf81b
sdrsatti <sdrsatti@gmail.com>
2023-04-12 16:54:21 -0400

    First

--- CardiologyStreamlit.py
+++ CardiologyStreamlit.py
@@ -27,24 +27,6 @@ PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_API_ENV = os.getenv('PINECONE_API_ENV')
 
 
-import base64
-
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-
-
 def getanswer(question):
     # initialize pinecone
     pinecone.init(
@@ -68,7 +50,6 @@ def getanswer(question):
 
 st.set_page_config(page_title='Cardiology ChatGPT', page_icon=':robot')
 
-add_bg_from_local('bluegrad.jpg')
 
 st.header('Cardiology ChatGPT')
 st.write('by S. D. Satti, MD, FACC, FHRS - me@sattimd.com')
